7ce8b0401d228c1bfdcec21b5abf37b371544afc
NobodyintheVoid <ya.a.matveev@gmail.com>
2026-04-03 10:33:05 +0700

    Add files via upload

---
+++ FireDragon.h
@@ -0,0 +1,15 @@
+#ifndef MEGAKNIGHTSVSTERRIBLEDRAGONS_SWORDANDSHIELD_H
+#define MEGAKNIGHTSVSTERRIBLEDRAGONS_SWORDANDSHIELD_H
+#include "Dragons.h"
+
+
+class FireDragon : public Dragons {
+public:
+    FireDragon() = default;
+    FireDragon(bool withElectro);
+    std::string NameDragon = "Fire Dragon";
+    int VSSAS = -1;
+    int VSC = 1;
+    int VSA = 0;
+    void Electro(int& rounds) override;
+};
